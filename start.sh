c50fa128f7da31cdac3542f0d3f4c98d658b06a2
sravantony2 <71973948+sravantony2@users.noreply.github.com>
2021-05-29 10:31:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 if [[ -n $RCLONE_CONFIG ]]; then
  echo "Rclone config detected"
  echo -e "$RCLONE_CONFIG" > /app/rclone.conf
+
  sed -i'' -e "/bt-tracker=/d" $(pwd)/aria.conf
  tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
  tracker_list=`curl -Ns https://trackerslist.com/all_aria2.txt | awk '$1' | tr '\n' ',' | cat`
