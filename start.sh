8017d582fbafa51de2a105131238b0cd55aac6de
sravantony2 <71973948+sravantony2@users.noreply.github.com>
2021-05-29 07:51:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ elif
 
 sed -i'' -e "/bt-tracker=/d" $(pwd)/aria.conf
 tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
+tracker_list=`curl -Ns https://trackerslist.com/all_aria2.txt | awk '$1' | tr '\n' ',' | cat`
+tracker_list=`curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat`
+tracker_list=`curl -Ns https://raw.githubusercontent.com/iamLiquidX/trackerlistx/main/trackers.txt | awk '$1' | tr '\n' ',' | cat`
 echo -e "bt-tracker=$tracker_list" >> $(pwd)/aria.conf
 
 fi
